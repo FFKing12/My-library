d0b31c5b0f595a1f174e173396ef9e897eb80dc7
FFKing12 <77181522+FFKing12@users.noreply.github.com>
2021-01-21 08:12:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+/*var mr,fr;
+
+
+
+function setup() {
+  createCanvas(800,400);
+  mr = createSprite(400, 200, 50, 50);
+  fr = createSprite(300, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  mr.x = World.mouseX;
+  mr.y = World.mouseY;
+
+  if(mr.x - fr.x < mr.width/2 + fr.width/2 && 
+    fr.x - mr.x < mr.width/2 + fr.width/2 &&
+    mr.y - fr.y < mr.height/2 + fr.height/2 && 
+      fr.y - mr.y < mr.height/2 + fr.height/2){
+   mr.shapeColor = "green";
+   fr.shapeColor = "green";
+  }
+  else{
+    mr.shapeColor = "red";
+    fr.shapeColor = "red";
+  }
+  drawSprites();
+}
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  gameObject1 = createSprite(100, 100, 50, 50);
+  gameObject1.shapeColor = "green";
+  gameObject2 = createSprite(200, 100, 50, 50);
+  gameObject2.shapeColor = "green";
+  gameObject3 = createSprite(300, 100, 50, 50);
+  gameObject3.shapeColor = "green";
+  gameObject4 = createSprite(400, 100, 50, 50);
+  gameObject4.shapeColor = "green";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect, gameObject1)){
+    movingRect.shapeColor = "blue";
+    gameObject1.shapeColor = "blue";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+  drawSprites();
+}*/
+
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bounceOff(movingRect,fixedRect);
+  drawSprites();
+}
+
